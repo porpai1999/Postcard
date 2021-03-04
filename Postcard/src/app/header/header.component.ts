30089a01e6c66c70c0f51f8fb4a48a8cf568cf7f
narathip15031999 <naratihporiginal@gmail.com>
2021-03-05 01:54:46 +0700

    oil

--- Postcard/src/app/header/header.component.ts
+++ Postcard/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ViewEncapsulation} from '@angular/core';
 import {MenuItem} from 'primeng/api';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-header',
@@ -8,29 +9,11 @@ import {MenuItem} from 'primeng/api';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
   constructor() { }
-  items: any;
+  
   ngOnInit(): void {
-    this.items = [
-      {
-          label: 'Update',
-          icon: 'pi pi-refresh'
-      },
-      {
-          label: 'Delete',
-          icon: 'pi pi-times'
-      },
-      {
-          label: 'Angular Website',
-          icon: 'pi pi-external-link',
-          url: 'http://angular.io'
-      },
-      {
-          label: 'Router',
-          icon: 'pi pi-upload',
-          routerLink: '/fileupload'
-      }
-  ];
+
   }
 
 }
