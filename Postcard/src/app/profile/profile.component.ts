d2d93fdf33b929f72e4ef4bceec65261a3304d30
narathip15031999 <naratihporiginal@gmail.com>
2021-03-05 12:01:34 +0700

    oil

--- Postcard/src/app/profile/profile.component.ts
+++ Postcard/src/app/profile/profile.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
 
